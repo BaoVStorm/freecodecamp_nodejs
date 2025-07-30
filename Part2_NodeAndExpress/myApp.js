2123dce76358d053ec736f1774892b03848d1400
VStorm <tranvubao2004@gmail.com>
2025-07-30 18:03:37 +0700

    add route with middleware

--- Part2_NodeAndExpress/myApp.js
+++ Part2_NodeAndExpress/myApp.js
@@ -4,13 +4,25 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
-//
+// middleware
 app.use('/public', express.static(__dirname + '/public'));
 
-// middleware
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next(); // không có sẽ request mãi, trình duyệt ko load
+});
+
+
+// --------------------------------- 
+
+// routes with middleware
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({
+        time: req.time
+    })
 });
 
 app.get('/', (req, res) =>{
@@ -18,7 +30,6 @@ app.get('/', (req, res) =>{
 });
 
 app.get("/json", (req, res) => {
-
     const text = 'Hello json';
     res.json({
         "message": (process.env.MESSAGE_STYLE === 'uppercase' ? text.toLocaleUpperCase() : text),
